362c642f9bad038c2b9445da3376c7ef11bbecf7
Rushikesh Korgaonkar <rushikesh.korgaonkar@gmail.com>
2020-10-09 01:20:47 +0700

    challenge-1

---
+++ challenge-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenge_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("aaab : " + IsAbeforeB("aaab"));
+            Console.WriteLine("ba : " + IsAbeforeB("ba"));
+            Console.WriteLine("aaa : " + IsAbeforeB("aaa"));
+            Console.WriteLine("b : " + IsAbeforeB("b"));
+            Console.WriteLine("abba : " + IsAbeforeB("abba"));
+            Console.WriteLine("baa : " + IsAbeforeB("baa"));
+            Console.ReadLine();
+        }
+
+        private static bool IsAbeforeB(string str)
+        {
+            return (str.IndexOf("a") < 0 || str.IndexOf("b") < 0 || !str.Contains("ba"));
+        }
+    }
+}
---
+++ challenge-1/readme.txt
@@ -0,0 +1,15 @@
+﻿How to write a function solution that, given a String S consisting of N letters 'a' or/and 'b' 
+returns true when occurences of 'a' are before 'b' and return false otherwise?
+
+The example results would be:
+
+Given S 'aaab', the function should return true.
+Given S 'ba', the function should return false.
+Given S 'aaa', the function should return true. 'b' does not need to occur in S
+Given S 'b', the function should return true. 'a' does not need to occur in S
+Given S 'abba', the function should return false.
+Given S 'baa', the function should return false.
+
+Write an efficient algorithm for the following assumptions:
+N is an integer within the range [1..300,000];
+string S consists only of the characters 'a' and/or 'b'
